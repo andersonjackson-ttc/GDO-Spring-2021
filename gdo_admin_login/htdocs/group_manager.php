bffd70edb2f0bf78ea05d96cecb2dbd2beb7ce00
TylerWard412 <tylerward25842@my.tridenttech.edu>
2021-04-20 21:02:44 -0400

    Added group addition and removal functions and moved the code that executes to the top of the page so that updates to the database would be reflected on the page when they are finished.

--- gdo_admin_login/htdocs/group_manager.php
+++ gdo_admin_login/htdocs/group_manager.php
@@ -5,7 +5,26 @@
     include_once("includes/header.php");
 	$page_title = 'Manage Groups'; 
 	include_once ('includes/frame.html');
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if(isset($_POST["removalquery"])){
+			$groupname = $_POST["removalquery"];
+            $removeGroup = mysqli_query($dbc, "DELETE FROM group_names WHERE group_name='$groupname'");
+            mysqli_query($dbc, $removeGroup);
+            echo 'Group Removed.';
+		}
+		if(isset($_POST["newgroup"])){
+			$groupname = $_POST["newgroup"];
+            $addGroup = mysqli_query($dbc, "INSERT INTO group_names (group_name) VALUES ('$groupname')");
+            mysqli_query($dbc, $addGroup);
+            echo 'Group Added.';
+		}
+		if(isset($_POST["maxgroupsize"])){
+			echo 'Functionality of group mass assignment is a work in progress';
+		//Add function to features below or figure out a better way to implement features
+		}
+		
 
+	}
 ?>
 	<!-- Unfinished, to be implemented -->
 	<div class="col" align="center">
@@ -14,7 +33,7 @@
     <form class="form-inline row justify-content-center" action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>" method="post">
         <div class="form-group  px-2">
             <label for="group_removal" style="padding-right: 1em">Remove</label>
-            <select class="form-control" id="group_removal" name="query">
+            <select class="form-control" id="group_removal" name="removalquery">
                 <option value="none" selected disabled hidden>Select an Option</option>
 <?php
 
@@ -59,11 +78,7 @@
 
 
 <?php
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		echo 'Functionality is a work in progress';
-		//Add function to features below or figure out a better way to implement features
 
-	}
 
 include_once("includes/footer.html");
 ?>
